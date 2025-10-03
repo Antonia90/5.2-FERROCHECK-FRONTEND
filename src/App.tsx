9b7001337d26ad06d8ab6ac168da4bfb819afa8a
Antonia90 <antominnucci@gmail.com>
2025-10-03 14:59:07 +0200

    add register route

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 export default function App() {
   return (
     <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </Layout>
-  )
+  );
 }
-
