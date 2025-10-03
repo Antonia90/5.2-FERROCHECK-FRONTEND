3e861424f55b2528dbd4d1550e6144904dbd9fcf
Antonia90 <antominnucci@gmail.com>
2025-10-03 16:06:04 +0200

    add ingredients route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Ingredients from "./pages/Ingredients";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/ingredients" element={<Ingredients />} />
       </Routes>
     </Layout>
   );
